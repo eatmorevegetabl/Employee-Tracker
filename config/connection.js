83efac0a29bac37229f208d16d52c59b79024bd2
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2023-01-19 04:55:09 -0700

    removed comments

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,7 @@ require('dotenv').config();
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    // Your MySQL username
     user: process.env.DB_USER,
-    // Your MySQL password
     password: process.env.DB_PW,
     database: 'company_db',
   },
